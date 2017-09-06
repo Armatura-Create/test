8bf4fa9fddc3df0d6a5371545f062d0984b9fb14
bapm91 <Kulynych.v.a@gmail.com>
2017-09-06 15:02:47 +0300

    v. 1.1

--- bmi/BMI.java
+++ bmi/BMI.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class BMI {
     public static void main(String args[]){
+	System.out.println("Hello");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your height: "); // in meters
         float height = sc.nextFloat();
